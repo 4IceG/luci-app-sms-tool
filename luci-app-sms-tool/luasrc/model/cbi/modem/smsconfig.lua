d372121973eb9b2f1786eb6f0f56644372ff08dd
4IceG <IceClubber@wp.pl>
2020-08-13 16:37:02 +0200

    Update smsconfig.lua

--- luci-app-sms-tool/luasrc/model/cbi/modem/smsconfig.lua
+++ luci-app-sms-tool/luasrc/model/cbi/modem/smsconfig.lua
@@ -125,9 +125,14 @@ end
 
 local time = s:option(Value, "checktime", translate("Check inbox every minute(s)"), "" .. translate("Specify how many minutes you want your inbox to be checked."))
 time.rmempty = true
-time.maxlength = 1
+time.maxlength = 2
 time.default = 0
 
+function time.validate(self, value)
+	if ( tonumber(value) < 60 and tonumber(value) > 1 ) then
+	return value
+	end
+end
 
 leds = s:option(Value, "smsled", translate("Notification LED"), "" .. translate("Select the notification LED."))
 if try_leds then
